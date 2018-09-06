600c254e138c6c5af8114d2908ed60fbcaac0e4e
kolyan <vkont4@gmail.com>
2018-09-06 14:00:00 +0200

    Added run mojo

---
+++ src/main/java/io/github/yashchenkon/sqs/SqsRunMojo.java
@@ -0,0 +1,18 @@
+package io.github.yashchenkon.sqs;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * @author Mykola Yashchenko
+ */
+@Mojo(name = "run")
+public class SqsRunMojo extends AbstractMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    }
+}
