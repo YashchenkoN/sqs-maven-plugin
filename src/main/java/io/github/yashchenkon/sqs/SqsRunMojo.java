b645796530d49c9d701b7afb936708d7c39a5b2d
kolyan <vkont4@gmail.com>
2018-09-10 14:00:00 +0200

    Added port config

--- src/main/java/io/github/yashchenkon/sqs/SqsRunMojo.java
+++ src/main/java/io/github/yashchenkon/sqs/SqsRunMojo.java
@@ -4,12 +4,16 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  * @author Mykola Yashchenko
  */
 @Mojo(name = "run")
 public class SqsRunMojo extends AbstractMojo {
+
+    @Parameter(property = "port", defaultValue = "8888")
+    private Integer port;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
