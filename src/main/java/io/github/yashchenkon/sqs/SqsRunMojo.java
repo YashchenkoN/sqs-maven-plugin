d88f9e3d54ad96e2f8aa90abbfc3cdada89dfe33
kolyan <vkont4@gmail.com>
2018-09-11 14:00:00 +0200

    Added server creation

--- src/main/java/io/github/yashchenkon/sqs/SqsRunMojo.java
+++ src/main/java/io/github/yashchenkon/sqs/SqsRunMojo.java
@@ -5,6 +5,9 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.elasticmq.NodeAddress;
+import org.elasticmq.rest.sqs.SQSRestServer;
+import org.elasticmq.rest.sqs.SQSRestServerBuilder;
 
 /**
  * @author Mykola Yashchenko
@@ -17,6 +20,7 @@ public class SqsRunMojo extends AbstractMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-
+        final SQSRestServer server = SQSRestServerBuilder.withPort(port)
+                .withServerAddress(new NodeAddress("http", "localhost", port, "")).start();
     }
 }
