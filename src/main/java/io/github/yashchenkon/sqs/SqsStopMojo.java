cfe3c39f9250a0a687402fe774c2551892402c85
kolyan <vkont4@gmail.com>
2018-09-15 14:00:00 +0200

    Stop server

--- src/main/java/io/github/yashchenkon/sqs/SqsStopMojo.java
+++ src/main/java/io/github/yashchenkon/sqs/SqsStopMojo.java
@@ -4,6 +4,7 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.elasticmq.rest.sqs.SQSRestServer;
 
 /**
  * @author Mykola Yashchenko
@@ -13,6 +14,9 @@ public class SqsStopMojo extends AbstractMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-
+        final SQSRestServer server = (SQSRestServer) getPluginContext().get(Constants.SERVER);
+        if (server != null) {
+            server.stopAndWait();
+        }
     }
 }
