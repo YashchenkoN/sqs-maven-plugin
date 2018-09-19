51759dce6229656543e693c71825868e28a17153
kolyan <vkont4@gmail.com>
2018-09-19 14:00:00 +0200

    Refactoring

--- src/main/java/io/github/yashchenkon/sqs/SqsRunMojo.java
+++ src/main/java/io/github/yashchenkon/sqs/SqsRunMojo.java
@@ -1,8 +1,6 @@
 package io.github.yashchenkon.sqs;
 
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.elasticmq.NodeAddress;
